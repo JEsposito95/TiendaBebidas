d489dfa0e85ad41c799b08e546ebbff25f498436
Joaquin Esposito <joaquiesposito@gmail.com>
2024-11-25 23:08:20 -0300

    inicializando

---
+++ src/main/java/com/bebidas/tiendaBebidas/TiendaBebidasApplication.java
@@ -0,0 +1,13 @@
+package com.bebidas.tiendaBebidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBebidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBebidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bebidas/tiendaBebidas/TiendaBebidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebidas.tiendaBebidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBebidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
